3a0af895fcdc6c58d1f0c5cf20aab6c7d538a84a
Xerneax8 <mosmalopez@gmail.com>
2025-05-08 22:12:58 +0200

    Update ej5

--- src/ejercicio_5.py
+++ src/ejercicio_5.py
@@ -6,11 +6,6 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
-# Leer el archivo de datos ya clasificados
-with open("../docs/data_clasified.json", "r") as file:
-    data = json.load(file)["tickets_emitidos"]
-
-
 # Preprocesamiento: transformar los datos a una tabla estructurada
 def extract_features(ticket):
     dias_abierto = (pd.to_datetime(ticket["fecha_cierre"]) - pd.to_datetime(ticket["fecha_apertura"])).days
@@ -30,34 +25,82 @@ def extract_features(ticket):
             "dias_abierto": dias_abierto,
         }
 
-# Sacar las caracterísiticas y crear un DataFrame con ello
-tickets = [extract_features(t) for t in data]
-df = pd.DataFrame(tickets)
+def linear_regression(X_train, y_train):
+    # Regresión lineal
+    reg = LinearRegression()
+    reg.fit(X_train, y_train)
 
-# Separar X e y
-X = df.drop("es_critico", axis=1)
-y = df["es_critico"]
+    # Predicción
+    y_pred_continuo = reg.predict(X_test)
+    y_pred = (y_pred_continuo > 0.5).astype(int)  # Umbral para convertir a clasificación
 
-# División de datos
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # Visualización
+    plt.scatter(range(len(y_test)), y_test, color="black", label="Reales")
+    plt.scatter(range(len(y_pred)), y_pred, color="blue", marker='x', label="Predichos (umbralizado)")
+    plt.title("Regresión Lineal sobre Tickets Críticos")
+    plt.xlabel("Índice de muestra")
+    plt.ylabel("¿Crítico?")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-# Regresión lineal
-reg = LinearRegression()
-reg.fit(X_train, y_train)
+    return reg
 
-# Predicción
-y_pred_continuo = reg.predict(X_test)
-y_pred = (y_pred_continuo > 0.5).astype(int)  # Umbral para convertir a clasificación
+def decision_tree(X_train, y_train):
+    # Decision Tree
+    tree_model = DecisionTreeClassifier()
+    tree_model.fit(X_train, y_train)
 
-# Visualización
-plt.scatter(range(len(y_test)), y_test, color="black", label="Reales")
-plt.scatter(range(len(y_pred)), y_pred, color="blue", marker='x', label="Predichos (umbralizado)")
-plt.title("Regresión Lineal sobre Tickets Críticos")
-plt.xlabel("Índice de muestra")
-plt.ylabel("¿Crítico?")
-plt.legend()
-plt.grid(True)
-plt.show()
+    # Mostrar el modelo
+    plt.figure(figsize=(12, 8))
+    plot_tree(
+        tree_model,
+        feature_names=X.columns,
+        class_names=['No crítico', 'Crítico'],
+        filled=False,
+        rounded=True,
+        proportion=True
+    )
+    plt.title('Árbol de Decisión (profundidad máxima = 3)')
+    plt.tight_layout()
+    plt.show()
+
+    return tree_model
+
+def randomForest(X_train, y_train):
+    # Random Forest
+    random_forest = RandomForestClassifier(max_depth=2, random_state=0,n_estimators=10)
+    random_forest.fit(X_train, y_train)
+
+
+    # Mostrar modelo
+    n_estimators = len(random_forest.estimators_)
+    cols = 5  # Número de columnas por fila
+    rows = (n_estimators + cols - 1) // cols  # Número de filas necesarias
+
+    fig, axes = plt.subplots(rows, cols, figsize=(cols * 4, rows * 4))
+    axes = axes.flatten()
+
+    for i, tree in enumerate(random_forest.estimators_):
+        plot_tree(
+            tree,
+            feature_names=X.columns,
+            class_names=['No crítico', 'Crítico'],
+            filled=True,
+            rounded=True,
+            proportion=True,
+            ax=axes[i]
+        )
+        axes[i].set_title(f'Árbol {i}')
+
+    # Eliminar subplots no usados si clf.n_estimators no es múltiplo de cols
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.tight_layout()
+    plt.show()
+
+    return random_forest
 
 # Función para crear tickets
 def creacion_ticket():
@@ -73,72 +116,48 @@ def creacion_ticket():
             "satisfaccion_cliente": satisfaccion, "tipo_incidencia": tipo_incidencia}
     a = extract_features(data)
     return pd.DataFrame(a, index=range(0, 1))
-# Predicción con Regresión Lineal
-a = creacion_ticket()
-y_pred = reg.predict(a)
 
-if y_pred[0] >= 0.5:
-    y_pred[0] = 1
-else:
-    y_pred[0] = 0
-print(y_pred)
 
-# Decision Tree
-tree_model = DecisionTreeClassifier()
-tree_model.fit(X_train, y_train)
+def prediccion(decision, ticket, reg, tree_model, random_forest):
+    result = None
+    # Predicción con Regresión Lineal
+    if decision == '0':
+        result = reg.predict(ticket)
 
-# Predicciones y evaluación
-b = creacion_ticket()
-y_pred = tree_model.predict(b)
+        if result[0] >= 0.5:
+            result[0] = 1
+        else:
+            result[0] = 0
 
-print(y_pred)
+    # Predicción con Decision Tree
+    elif decision == '1':
+        result = tree_model.predict(ticket)
+    # Predicción con Random Forest
+    elif decision == '2':
+        result = random_forest.predict(ticket)
 
-# Mostrar el modelo
-plt.figure(figsize=(12, 8))
-plot_tree(
-    tree_model,
-    feature_names=X.columns,
-    class_names=['No crítico', 'Crítico'],
-    filled=False,
-    rounded=True,
-    proportion=True
-)
-plt.title('Árbol de Decisión (profundidad máxima = 3)')
-plt.tight_layout()
-plt.show()
+    return result
 
-# Random Forest
-random_forest = RandomForestClassifier(max_depth=2, random_state=0,n_estimators=10)
-random_forest.fit(X_train, y_train)
+# Leer el archivo de datos ya clasificados
+with open("../docs/data_clasified.json", "r") as file:
+    data = json.load(file)["tickets_emitidos"]
 
-c = creacion_ticket()
-y_pred = random_forest.predict(c)
+# Sacar las caracterísiticas y crear un DataFrame con ello
+tickets = [extract_features(t) for t in data]
+df = pd.DataFrame(tickets)
 
-print(y_pred)
+# Separar X e y
+X = df.drop("es_critico", axis=1)
+y = df["es_critico"]
 
-# Mostrar modelo
-n_estimators = len(random_forest.estimators_)
-cols = 5  # Número de columnas por fila
-rows = (n_estimators + cols - 1) // cols  # Número de filas necesarias
+# División de datos
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-fig, axes = plt.subplots(rows, cols, figsize=(cols * 4, rows * 4))
-axes = axes.flatten()
+reg = linear_regression(X_train, y_train)
+tree_model = decision_tree(X_train, y_train)
+random_forest = randomForest(X_train, y_train)
 
-for i, tree in enumerate(random_forest.estimators_):
-    plot_tree(
-        tree,
-        feature_names=X.columns,
-        class_names=['No crítico', 'Crítico'],
-        filled=True,
-        rounded=True,
-        proportion=True,
-        ax=axes[i]
-    )
-    axes[i].set_title(f'Árbol {i}')
 
-# Eliminar subplots no usados si clf.n_estimators no es múltiplo de cols
-for j in range(i + 1, len(axes)):
-    fig.delaxes(axes[j])
-
-plt.tight_layout()
-plt.show()
+decision = '0'
+ticket = creacion_ticket()
+print(prediccion(decision, ticket, reg, tree_model, random_forest))
